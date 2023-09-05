c9235ba700839effcf16b2098d15730d9b81ac5f
Birahe <ardaaozyigit@gmail.com>
2023-09-05 22:15:32 +0300

    Config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias v=nvim
+starship init fish | source
+set fish_greeting 
+fish_add_path /home/birahe/.spicetify
